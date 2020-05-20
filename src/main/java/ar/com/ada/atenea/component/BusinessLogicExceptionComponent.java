005e689ec73736e185fb2856fd8fa1fdf7feeb41
Sofia <soofi.maidana@gmail.com>
2020-05-20 19:53:37 -0300

    base structure

---
+++ src/main/java/ar/com/ada/atenea/component/BusinessLogicExceptionComponent.java
@@ -0,0 +1,24 @@
+package ar.com.ada.atenea.component;
+
+import ar.com.ada.atenea.exception.ApiEntityError;
+import ar.com.ada.atenea.exception.BusinessLogicException;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Component;
+
+@Component("businessLogicExceptionComponent")
+public class BusinessLogicExceptionComponent {
+
+    public void throwExceptionEntityNotFound(String entityName, Long id) {
+        ApiEntityError apiEntityError = new ApiEntityError(
+                entityName,
+                "Not found",
+                "The " + entityName + " with id '" + id + "' does not exist"
+        );
+
+        throw new BusinessLogicException(
+                entityName + " does not exist",
+                HttpStatus.NOT_FOUND,
+                apiEntityError
+        );
+    }
+}
