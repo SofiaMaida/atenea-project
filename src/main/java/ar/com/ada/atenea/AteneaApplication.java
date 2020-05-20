2e11825f3ed0599c4e40ba3fdc5c6caf4859b5d0
Sofia <soofi.maidana@gmail.com>
2020-05-20 19:31:46 -0300

    initial project

---
+++ src/main/java/ar/com/ada/atenea/AteneaApplication.java
@@ -0,0 +1,13 @@
+package ar.com.ada.atenea;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AteneaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AteneaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ar/com/ada/atenea/AteneaApplicationTests.java
@@ -0,0 +1,13 @@
+package ar.com.ada.atenea;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AteneaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
